e09df75d7a349de227137f1be2b2a6e7149f396e
Diego Gabriel León Peña <diegabo1996@yahoo.es>
2022-06-22 21:57:29 -0400

    Agregar archivos de proyecto.

---
+++ Arrays/Models/Table.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays.Models
+{
+    public class Table
+    {
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public string Material { get; set; }
+        public byte NumberSupports { get; set; }
+        public Size Size { get; set; }
+
+    }
+    public enum Size
+    {
+        Small,
+        Medium,
+        Large
+    }
+}
---
+++ Arrays/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Arrays.Models;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cuantas mesas quieres en el arreglo?");
+            int numero = int.Parse(Console.ReadLine());
+            //This is the way for declare arrays
+            Table[] tables = new Table[numero];
+            //Auxiliar Varable
+            int i=0;
+            do
+            {
+                //Inicializacion de Objeto Individual
+                tables[i] = new Table();
+                Console.WriteLine("Cual es la marca de la mesa "+(i+1)+"?");
+                tables[i].Brand=Console.ReadLine();
+                Console.WriteLine("Cual es el color de la mesa " + (i + 1) + "?");
+                tables[i].Color = Console.ReadLine();
+                Console.WriteLine("Cual es el material de la mesa " + (i + 1) + "?");
+                tables[i].Material= Console.ReadLine();
+                Console.WriteLine("Cual es el numero de soportes de la mesa " + (i + 1) + "?");
+                tables[i].NumberSupports = byte.Parse(Console.ReadLine());
+                Console.WriteLine("Cual es el tamaño de la mesa " + (i + 1) + "?");
+                int aSize = 0;
+                foreach (Size size in (Size[])Enum.GetValues(typeof(Size)))
+                {
+                    Console.WriteLine(aSize + ") " + size.ToString());
+                    aSize++;
+                }
+                tables[i].Size = (Size)(int.Parse(Console.ReadLine()));
+                i=i+1;
+
+            } while (i < numero);
+            Console.WriteLine("Fin de programa!");
+
+        }
+    }
+}
